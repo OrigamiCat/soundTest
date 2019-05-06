e30d43442d4fb38fb06b37d898876dd9e9ea1347
BOB <g1727044@st.t-kougei.ac.jp>
2019-05-06 17:06:45 +0900

    隕石の音を確認するやつ

---
+++ testSound/Assets/GameMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    public float vector;
+    public GameObject Meteorite;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        this.transform.position += new Vector3(0, 0, vector);
+
+        if(this.transform.position.z < -100.0)
+        {
+            Destroy(this.gameObject);
+
+            Instantiate(Meteorite);
+        }
+    }
+}
---
+++ testSound/Assets/SoundCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCheck : MonoBehaviour
+{
+    public AudioClip se_1;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            audioSource.PlayOneShot(se_1);
+        }
+    }
+}
